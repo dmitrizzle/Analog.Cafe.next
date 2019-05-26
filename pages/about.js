33d3ea40234479abe9a5a60d57ba28df62734442
Dmitri <d@eurekaking.com>
2019-05-25 17:17:34 -0700

    First commit

---
+++ pages/about.js
@@ -0,0 +1,4 @@
+import Link from 'next/link'
+
+const About = () => <p>About <Link href="/"><a>Home</a></Link></p>
+export default About
---
+++ pages/index.js
@@ -0,0 +1,6 @@
+import Link from 'next/link'
+
+const Index = () => (
+    <p>Analog.Cafe <Link href="/about"><button>About</button></Link></p>
+)
+export default Index
