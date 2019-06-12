f3c6dce57dd916ee2831a0570137c4bd7a7788f2
Dmitri <d@eurekaking.com>
2019-06-11 19:24:41 -0700

    Start from 0

--- core/components/layouts/Main/components/AppStatusWrapper.js
+++ core/components/layouts/Main/components/AppStatusWrapper.js
@@ -3,7 +3,7 @@ import styled, { keyframes, css } from "styled-components";
 import { c_red, c_white_a0 } from "../../../../../constants/styles/colors";
 
 const progress = keyframes`
-  0% { transform: scale(.1,1) rotateZ(360deg) }
+  0% { transform: scale(0,1) rotateZ(360deg) }
   100% { transform: scale(.99,1) rotateZ(360deg);}
 `;
 
@@ -21,7 +21,7 @@ export const AnimatedProgress = styled.div`
       : css`
           ${progress} 10s cubic-bezier(0, 0.9, 0.75, 1) forwards;
         `};
-  transform: scale(${props => (props.isInert ? 1 : 0.1)}, 1) rotateZ(360deg);
+  transform: scale(${props => (props.isInert ? 1 : 0)}, 1) rotateZ(360deg);
 
   background: ${props => (props.isInert ? c_white_a0 : c_red)};
   transition: background ${props => (props.isInert ? 0.75 : 0)}s, width 150ms;
