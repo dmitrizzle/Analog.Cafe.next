c21baf595887f7a1518064f2f1f196f2e64202a4
Dmitri <d@eurekaking.com>
2019-06-14 00:52:56 -0700

    Add spacer

--- core/components/vignettes/HeaderLarge/components/HeaderWrapper.js
+++ core/components/vignettes/HeaderLarge/components/HeaderWrapper.js
@@ -3,12 +3,10 @@ import styled from "styled-components";
 import { c_black } from "../../../../../constants/styles/colors";
 import { m_column } from "../../../../../constants/styles/measurements";
 
-const sp = 1.5;
-
 export default styled.header`
   text-align: center;
-  margin: ${sp}em auto 0;
+  margin: 1.5em auto 0;
   max-width: ${m_column};
   border-bottom: 8px solid ${c_black};
-  padding: 0 ${sp}em 0;
+  padding: 0 1.5em 0.5em;
 `;
