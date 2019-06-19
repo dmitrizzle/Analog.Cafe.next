fec756f97da178e1cd3fc912f271761d9a4fdd72
Dmitri <d@eurekaking.com>
2019-06-19 14:29:10 -0700

    Hide topics from menu

--- core/components/controls/Menu/constants.js
+++ core/components/controls/Menu/constants.js
@@ -7,7 +7,7 @@ import topics from "../Topics";
 
 export const MENU_BUTTONS = props => [
   {
-    mobile: "on",
+    hidden: true,
     to: "/nav/topics",
     onClick: event => {
       event.preventDefault();
