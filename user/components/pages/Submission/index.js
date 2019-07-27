212123ae04df7a99689202b75fd3f469e41450e5
Dmitri <d@eurekaking.com>
2019-07-27 16:33:00 -0700

    Adjust styled components

--- user/components/pages/Submission/index.js
+++ user/components/pages/Submission/index.js
@@ -1,53 +1,57 @@
 import React from "react";
 import styled from "styled-components";
 
-import { c_grey_dark } from "../../../../constants/styles/colors";
+import { c_grey_dark, c_grey_light } from "../../../../constants/styles/colors";
 import { dateFromUnix } from "../../../../utils/time";
-import { title } from "../../../../constants/styles/typography";
+import CardHeader from "../../../../core/components/controls/Card/components/CardHeader";
+import CardIntegrated from "../../../../core/components/controls/Card/components/CardIntegrated";
 import Link from "../../../../core/components/controls/Link";
 
 const Table = styled.table`
   color: ${c_grey_dark};
-  margin: 0 auto;
+  width: 100%;
+  border-radius: 0.25em;
+  overflow: hidden;
+  tr:nth-child(even) {
+    background: ${c_grey_light};
+  }
 `;
-const TableHeader = styled.th`
-  ${title}
-`;
+
 const TableCell = styled.td`
   font-size: 0.8em;
   line-height: 1em;
-  padding: 0.25em 0.5em;
+  padding: 0.5em 1em;
 `;
 const TableCellDate = styled(TableCell)`
-  width: 10em;
+  width: 13em;
 `;
 
 export const Edits = ({ article }) => {
   return article && article.edits ? (
-    <Table>
-      <tbody>
-        <tr>
-          <TableHeader colSpan="2">Edit History</TableHeader>
-        </tr>
-        {article.edits.map(edit => {
-          const unix = new Date(edit.date * 1000);
+    <CardIntegrated>
+      <CardHeader stubborn buttons={[0]} noStar title="Edit History" />
+      <Table>
+        <tbody>
+          {article.edits.map(edit => {
+            const unix = new Date(edit.date * 1000);
 
-          return (
-            <tr key={edit.date}>
-              <TableCellDate>
-                {dateFromUnix(edit.date).human +
-                  " " +
-                  unix.getHours() +
-                  ":" +
-                  unix.getMinutes()}
-              </TableCellDate>
-              <TableCell>
-                <Link to={`/u/${edit.id}`}>{edit.name}</Link>
-              </TableCell>
-            </tr>
-          );
-        })}
-      </tbody>
-    </Table>
+            return (
+              <tr key={edit.date}>
+                <TableCellDate>
+                  {dateFromUnix(edit.date).human +
+                    " " +
+                    unix.getHours() +
+                    ":" +
+                    unix.getMinutes()}
+                </TableCellDate>
+                <TableCell>
+                  <Link to={`/u/${edit.id}`}>{edit.name}</Link>
+                </TableCell>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
+    </CardIntegrated>
   ) : null;
 };
