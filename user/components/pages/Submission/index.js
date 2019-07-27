e858276a11d7b57e55344602017d35c0ae62aa63
Dmitri <d@eurekaking.com>
2019-07-27 16:54:55 -0700

    Refactor and fs table

--- user/components/pages/Submission/index.js
+++ user/components/pages/Submission/index.js
@@ -1,57 +1,32 @@
 import React from "react";
-import styled from "styled-components";
 
-import { c_grey_dark, c_grey_light } from "../../../../constants/styles/colors";
+import { Table, TableCell, TableRow } from "../../vignettes/Table";
 import { dateFromUnix } from "../../../../utils/time";
-import CardHeader from "../../../../core/components/controls/Card/components/CardHeader";
-import CardIntegrated from "../../../../core/components/controls/Card/components/CardIntegrated";
+import ArticleSection from "../../../../core/components/pages/Article/components/ArticleSection";
 import Link from "../../../../core/components/controls/Link";
-
-const Table = styled.table`
-  color: ${c_grey_dark};
-  width: 100%;
-  border-radius: 0.25em;
-  overflow: hidden;
-  tr:nth-child(even) {
-    background: ${c_grey_light};
-  }
-`;
-
-const TableCell = styled.td`
-  font-size: 0.8em;
-  line-height: 1em;
-  padding: 0.5em 1em;
-`;
-const TableCellDate = styled(TableCell)`
-  width: 13em;
-`;
 
 export const Edits = ({ article }) => {
   return article && article.edits ? (
-    <CardIntegrated>
-      <CardHeader stubborn buttons={[0]} noStar title="Edit History" />
-      <Table>
-        <tbody>
-          {article.edits.map(edit => {
-            const unix = new Date(edit.date * 1000);
-
-            return (
-              <tr key={edit.date}>
-                <TableCellDate>
-                  {dateFromUnix(edit.date).human +
-                    " " +
-                    unix.getHours() +
-                    ":" +
-                    unix.getMinutes()}
-                </TableCellDate>
-                <TableCell>
-                  <Link to={`/u/${edit.id}`}>{edit.name}</Link>
-                </TableCell>
-              </tr>
-            );
-          })}
-        </tbody>
+    <ArticleSection>
+      <Table header="Edit History">
+        {article.edits.map(edit => {
+          const unix = new Date(edit.date * 1000);
+          return (
+            <TableRow key={edit.date}>
+              <TableCell>
+                {dateFromUnix(edit.date).human +
+                  " " +
+                  unix.getHours() +
+                  ":" +
+                  unix.getMinutes()}
+              </TableCell>
+              <TableCell>
+                <Link to={`/u/${edit.id}`}>{edit.name}</Link>
+              </TableCell>
+            </TableRow>
+          );
+        })}
       </Table>
-    </CardIntegrated>
+    </ArticleSection>
   ) : null;
 };
---
+++ user/components/vignettes/Table/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from "styled-components";
+
+import { c_grey_dark, c_grey_light } from "../../../../constants/styles/colors";
+import CardHeader from "../../../../core/components/controls/Card/components/CardHeader";
+import CardIntegrated from "../../../../core/components/controls/Card/components/CardIntegrated";
+
+const TableStyled = styled.table`
+  color: ${c_grey_dark};
+  width: 100%;
+  border-radius: 0.25em;
+  overflow: hidden;
+  tr:nth-child(even) {
+    background: ${c_grey_light};
+  }
+`;
+
+export const TableRow = styled.tr``;
+export const TableCell = styled.td`
+  font-size: 0.8em;
+  line-height: 1em;
+  padding: 0.5em 1em;
+`;
+export const Table = ({ header, children }) => {
+  return (
+    <CardIntegrated style={{ maxWidth: "100%" }}>
+      <CardHeader stubborn buttons={[0]} noStar title={header} />
+      <TableStyled>
+        <tbody>{children}</tbody>
+      </TableStyled>
+    </CardIntegrated>
+  );
+};
