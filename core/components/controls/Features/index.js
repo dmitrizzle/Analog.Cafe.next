4334c56c52fe6ae4a243e1e71ad18875d16a953c
dmitrizzle <8587882+dmitrizzle@users.noreply.github.com>
2019-08-21 18:33:04 -0700

    Mock posters and links

--- core/components/controls/Features/index.js
+++ core/components/controls/Features/index.js
@@ -1,5 +1,11 @@
 import styled from "styled-components";
 
+import {title} from "../../../../constants/styles/typography"
+import {c_white} from "../../../../constants/styles/colors"
+import { makeFroth } from "../../../../utils/froth";
+import Link from "../Link"
+
+
 const Wall = styled.div`
   height: 16em;
   margin-bottom: 1.5em;
@@ -7,16 +13,31 @@ const Wall = styled.div`
   overflow: scroll;
   -webkit-overflow-scrolling: touch;
 `;
-const Poster = styled.div`
+const Poster = styled(Link)`
+  position: relative;
+  display: block;
+
   width: 10em;
   height: 16em;
   background: #eee;
   border-radius: 0.25em;
   margin-left: 1em;
   flex-shrink: 0;
+  background-size: cover !important;
+  background-position: center !important;
 
   &:first-child {
     margin-left: 1.5em;
+  }
+
+  h4 {
+    ${title}
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    padding: .5em;
+    text-align: right;
+    color: ${c_white};
   }
 `;
 const Spacer = styled.div`
@@ -27,10 +48,22 @@ const Spacer = styled.div`
 
 export default () => (
   <Wall>
-    <Poster />
-    <Poster />
-    <Poster />
-    <Poster />
+    <Poster to="/r/where-to-develop-film-in-vancouver-qrww" style={{backgroundImage: `url(${makeFroth({
+      src: "image-froth_1502233_rk5xNfD-H",
+      size: "m",
+    }).src})`}}><h4>Where to Develop Film in Vancouver</h4></Poster>
+    <Poster to="/r/june-19-community-letter-qi48" style={{backgroundImage:`url(${makeFroth({
+      src: "image-froth_1424197_HkHi_M4-r",
+      size: "m",
+    }).src})`}}><h4>June ‘19 Community Letter</h4></Poster>
+    <Poster to="/r/the-monobath-experience-ynpg" style={{backgroundImage:`url(${makeFroth({
+      src: "image-froth_1586967_ryAWgQxA4",
+      size: "m",
+    }).src})`}} ><h4>The Monobath Experience</h4></Poster>
+    <Poster to="/r/film-photography-costs-and-prices-kd5j" style={{backgroundImage:`url(${makeFroth({
+      src: "image-froth_1523727_SJoQ3JD9X",
+      size: "m"
+    }).src})`}}><h4>Film Photography Costs and Prices</h4></Poster>
     <Poster />
     <Poster />
     <Poster />
