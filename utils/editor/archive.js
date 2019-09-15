64e9d9faba3b25114bc1fdfb234ea8867ee3c9ed
dmitrizzle <8587882+dmitrizzle@users.noreply.github.com>
2019-09-14 18:37:24 -0700

    Can archive

--- utils/editor/archive.js
+++ utils/editor/archive.js
@@ -1,3 +1,6 @@
+import { API } from "../../constants/router/defaults";
+import puppy from "../puppy";
+
 export default (event, props) => {
   event.preventDefault();
 
@@ -10,4 +13,61 @@ export default (event, props) => {
     },
     id: "error/archive",
   };
+
+  // archive action
+  const archive = id => {
+    const request = {
+      url: `${API.SUBMISSIONS}/${id}`,
+      method: "delete",
+      headers: {
+        Authorization: "JWT " + localStorage.getItem("token"),
+      },
+    };
+    puppy(request)
+      .then(r => {
+        // reject and show error message if response status isn't success
+        if (r.status !== 200) {
+          r.reject();
+          return props.setModal(errorMessage);
+        }
+        return r.json();
+      })
+      .then(() => {
+        return props.setModal({
+          status: "ok",
+          info: {
+            title: "Archived!",
+          },
+          id: "notification/archive-success",
+        });
+      })
+      .catch(() => props.setModal(errorMessage));
+  };
+
+  props.setModal({
+    status: "ok",
+    info: {
+      title: "Archive?",
+      text:
+        "Please confirm that you want to archive this submission. You can not undo this action without admin access.",
+      buttons: [
+        {
+          to: "#confirm-archive",
+          onClick: event => {
+            event.preventDefault();
+            archive(props.article.id);
+          },
+          text: "Confirm Archive",
+          branded: true,
+        },
+        {
+          to: "#cancel",
+          onClick: event => {
+            event.preventDefault();
+          },
+          text: "Cancel",
+        },
+      ],
+    },
+  });
 };
