5b999b0d7229fa179388d700f18f8bc01cde2d9f
dmitrizzle <8587882+dmitrizzle@users.noreply.github.com>
2019-09-28 16:39:42 -0700

    Remove unused code, produce download page with working link

--- core/components/pages/Article/components/ArticleBlock.js
+++ core/components/pages/Article/components/ArticleBlock.js
@@ -2,6 +2,12 @@ import React from "react";
 import Reader from "@roast-cms/french-press-editor/dist/components/vignettes/Reader";
 
 import { AuthorsPrinted } from "./AuthorsPrinted";
+import {
+  DocketResponsive,
+  DocketResponsiveImage,
+  DocketResponsiveInfo,
+} from '../../List/components/DocketResponsive';
+import { LabelWrap } from '../../../controls/Docket';
 import { c_grey_dark } from "../../../../../constants/styles/colors";
 import { readingTime } from "../../../../../utils/time";
 import ArticleFooter from "./ArticleFooter";
@@ -9,11 +15,33 @@ import ArticleNav from "./ArticleNav";
 import ArticleSection from "./ArticleSection";
 import ArticleWrapper from "./ArticleWrapper";
 import HeaderLarge from "../../../vignettes/HeaderLarge";
+import Label from '../../../vignettes/Label';
 import Link from "../../../controls/Link";
+import LinkButton from '../../../controls/Button/components/LinkButton';
 import Main from "../../../layouts/Main";
 import Picture from "../../../vignettes/Picture";
 
 export const ArticleBlock = props => {
+  const isDownload = props.article.tag === "download";
+  let downloadLink = "/account"
+
+  // source the link for download (it'll grab the first link in the content)
+  // pagagraph > link
+  if(isDownload){
+    const {nodes} = props.article.content.raw.document;
+    nodes.forEach(node => {
+      if(node.type === "paragraph"){
+        node.nodes.forEach(subNode => {
+          if(subNode.type === "link"){
+            downloadLink = subNode.data.href;
+            return;
+          }
+        })
+        return;
+      }
+    })
+  }
+
   return (
     <Main>
       <ArticleNav
@@ -23,7 +51,7 @@ export const ArticleBlock = props => {
         }}
       />
       <ArticleWrapper>
-        <HeaderLarge
+        {!isDownload ? <HeaderLarge
           pageTitle={props.article.title}
           pageSubtitle={props.article.subtitle}
         >
@@ -33,9 +61,10 @@ export const ArticleBlock = props => {
               <AuthorsPrinted authors={props.article.authors} shouldLink />.
             </small>
           </em>
-        </HeaderLarge>
-        <ArticleSection>
-          <Reader
+        </HeaderLarge> : <HeaderLarge pageTitle={props.user.status === "ok" ? "Your Download is Ready" : "Please Sign In"} />}
+
+        <ArticleSection>{!isDownload ?
+        <><Reader
             value={props.article.content.raw}
             components={{ Picture, Link }}
           />
@@ -49,8 +78,29 @@ export const ArticleBlock = props => {
             thisArticleEditDate={
               props.article.date && props.article.date.updated
             }
-          />
-        </ArticleSection>
+          /></>
+        : <><div style={{ display: "flex", paddingTop: "1.5em" }}>
+        <DocketResponsive to={downloadLink} style={{
+          maxWidth: "32em",
+          margin: "0 auto"
+        }}>
+          <DocketResponsiveImage src={props.article.poster}  />
+          <DocketResponsiveInfo>
+            <h4>
+              {props.article.title}
+            </h4>
+            <small>
+              <em>{props.article.summary}</em>
+            </small>
+          </DocketResponsiveInfo>
+          <LabelWrap>
+              <Label blue>
+                download
+              </Label>
+          </LabelWrap>
+        </DocketResponsive></div>
+        <LinkButton branded to={downloadLink}>Download Now</LinkButton>
+        </>}</ArticleSection>
       </ArticleWrapper>
     </Main>
   );
