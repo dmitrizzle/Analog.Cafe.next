a594260025a8b05ae09a7de5a442897436d4d90b
dmitrizzle <8587882+dmitrizzle@users.noreply.github.com>
2019-09-30 19:26:13 -0700

    Clean up

--- core/components/pages/List/components/ListBlock.js
+++ core/components/pages/List/components/ListBlock.js
@@ -1,5 +1,5 @@
 import LazyLoad from "react-lazyload";
-import React, { useEffect } from "react";
+import React from "react";
 
 import { AuthorsPrinted } from "../../Article/components/AuthorsPrinted";
 import {
