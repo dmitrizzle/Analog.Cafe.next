89d71dda9963e5671dda4bbf192499ad79eca3d9
dmitrizzle <8587882+dmitrizzle@users.noreply.github.com>
2019-10-10 19:45:58 -0700

    Clean up

--- core/components/pages/Article/components/ArticleCoffee.js
+++ core/components/pages/Article/components/ArticleCoffee.js
@@ -1,15 +1,7 @@
-import React, { useEffect } from "react";
-import styled from "styled-components";
+import React from "react";
 
 import { CoffeeInline } from "../../../icons/Coffee";
-import { b_laptop } from "../../../../../constants/styles/measurements";
-import {
-  c_black,
-  c_red,
-  c_white,
-} from "../../../../../constants/styles/colors";
 import { getFirstNameFromFull } from "../../../../../utils/author-credits";
-import { title } from "../../../../../constants/styles/typography";
 import Link from "../../../controls/Link";
 
 // NOTE: styles are in ArticleWrapper
