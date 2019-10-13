0eebf992c415e5d470ffd900e5d1b3f2c120ffde
dmitrizzle <8587882+dmitrizzle@users.noreply.github.com>
2019-10-13 16:30:40 -0700

    Fix disabled GA

--- next.config.js
+++ next.config.js
@@ -1,20 +1,23 @@
 const withCSS = require("@zeit/next-css");
+const withOffline = require("next-offline");
 
-module.exports = withCSS({
-  // eslint-disable-next-line
-  webpack(config, options) {
-    config.module.rules.push({
-      test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-      use: {
-        loader: "url-loader",
-        options: {
-          // source files from static dir
-          outputPath: "static/",
-          publicPath: "/_next/static/",
-          limit: 10000, // 10kb
+module.exports = withOffline(
+  withCSS({
+    // eslint-disable-next-line
+    webpack(config, options) {
+      config.module.rules.push({
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            // source files from static dir
+            outputPath: "static/",
+            publicPath: "/_next/static/",
+            limit: 10000, // 10kb
+          },
         },
-      },
-    });
-    return config;
-  },
-});
+      });
+      return config;
+    },
+  })
+);
--- utils/data/ga.js
+++ utils/data/ga.js
@@ -11,30 +11,31 @@ const create = new Promise(resolve => {
 
 export const initializeGA = () =>
   typeof localStorage !== "undefined"
-    ? create.then(ReactGA => {
-        ReactGA.initialize("UA-91374353-3", {
-          debug: process.env.NODE_ENV === "development",
-          titleCase: false,
-          gaOptions: {},
-          gaAddress: "/static/analytics-201808051558.js",
-        });
+    ? create.then(ga => {
+        ga &&
+          ga.initialize("UA-91374353-3", {
+            debug: process.env.NODE_ENV === "development",
+            titleCase: false,
+            gaOptions: {},
+            gaAddress: "/static/analytics-201808051558.js",
+          });
       })
     : null;
 
 export const pageviewGA = url => {
-  create.then(ReactGA => {
-    ReactGA.pageview(url || window.location.pathname + window.location.search);
+  create.then(ga => {
+    ga && ga.pageview(url || window.location.pathname + window.location.search);
   });
 };
 
 export const modalviewGA = loc => {
-  create.then(ReactGA => {
-    ReactGA.modalview(loc);
+  create.then(ga => {
+    ga && ga.modalview(loc);
   });
 };
 
 export const eventGA = options => {
-  create.then(ReactGA => {
-    ReactGA.event(options);
+  create.then(ga => {
+    ga && ga.event(options);
   });
 };
