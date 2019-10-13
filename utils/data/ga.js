e023f0975dd63b37e3c6f1e22ba223c2f3361f78
dmitrizzle <8587882+dmitrizzle@users.noreply.github.com>
2019-10-12 21:24:59 -0700

    Catch disabled ga

--- utils/data/ga.js
+++ utils/data/ga.js
@@ -8,30 +8,40 @@ const create = new Promise((resolve, reject) => {
 
 export const initializeGA = () =>
   typeof localStorage !== "undefined"
-    ? create.then(ReactGA => {
-        ReactGA.initialize("UA-91374353-3", {
-          debug: process.env.NODE_ENV === "development",
-          titleCase: false,
-          gaOptions: {},
-          gaAddress: "/static/analytics-201808051558.js",
-        });
-      })
+    ? create
+        .then(ReactGA => {
+          ReactGA.initialize("UA-91374353-3", {
+            debug: process.env.NODE_ENV === "development",
+            titleCase: false,
+            gaOptions: {},
+            gaAddress: "/static/analytics-201808051558.js",
+          });
+        })
+        .catch(() => {})
     : null;
 
 export const pageviewGA = url => {
-  create.then(ReactGA => {
-    ReactGA.pageview(url || window.location.pathname + window.location.search);
-  });
+  create
+    .then(ReactGA => {
+      ReactGA.pageview(
+        url || window.location.pathname + window.location.search
+      );
+    })
+    .catch(() => {});
 };
 
 export const modalviewGA = loc => {
-  create.then(ReactGA => {
-    ReactGA.modalview(loc);
-  });
+  create
+    .then(ReactGA => {
+      ReactGA.modalview(loc);
+    })
+    .catch(() => {});
 };
 
 export const eventGA = options => {
-  create.then(ReactGA => {
-    ReactGA.event(options);
-  });
+  create
+    .then(ReactGA => {
+      ReactGA.event(options);
+    })
+    .catch(() => {});
 };
