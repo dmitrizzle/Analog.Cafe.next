87e2d0c96663374473a8924b141158be463d2f92
dmitrizzle <8587882+dmitrizzle@users.noreply.github.com>
2019-10-14 11:06:49 -0700

    Add sw on dev

--- next.config.js
+++ next.config.js
@@ -1,29 +1,28 @@
-const withCSS = require("@zeit/next-css");
-const withOffline = require("next-offline");
+const css = require("@zeit/next-css");
+const offline = require("next-offline");
+const withPlugins = require("next-compose-plugins");
 
 const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+      use: {
+        loader: "url-loader",
+        options: {
+          // source files from static dir
+          outputPath: "static/",
+          publicPath: "/_next/static/",
+          limit: 10000, // 10kb
+        },
+      },
+    });
+    return config;
+  },
   generateInDevMode: true,
+  devSwSrc: "/service-worker.js",
   workboxOpts: {},
 };
 
-module.exports = withOffline(
-  withCSS({
-    nextConfig,
-    // eslint-disable-next-line
-    webpack(config, options) {
-      config.module.rules.push({
-        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-        use: {
-          loader: "url-loader",
-          options: {
-            // source files from static dir
-            outputPath: "static/",
-            publicPath: "/_next/static/",
-            limit: 10000, // 10kb
-          },
-        },
-      });
-      return config;
-    },
-  })
-);
+const cssConfig = {};
+
+module.exports = withPlugins([[offline], [css, cssConfig]], nextConfig);
