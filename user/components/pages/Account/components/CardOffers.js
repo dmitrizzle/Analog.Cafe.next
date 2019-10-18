551234f5fb7f5d3fb18147d8f12d56c7768f25d4
dmitrizzle <8587882+dmitrizzle@users.noreply.github.com>
2019-10-17 20:34:45 -0700

    Load offers from API

--- user/components/pages/Account/components/CardOffers.js
+++ user/components/pages/Account/components/CardOffers.js
@@ -1,51 +1,75 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
+import { API } from "../../../../../constants/router/defaults";
 import { CardIntegratedForColumns } from "../../../../../core/components/controls/Card/components/CardColumns";
 import { CardWithDocketsInfo } from "../../../../../core/components/controls/Card/components/CardWithDockets";
 import { c_yellow } from "../../../../../constants/styles/colors";
 import CardHeader from "../../../../../core/components/controls/Card/components/CardHeader";
 import Link from "../../../../../core/components/controls/Link";
 import LinkButton from "../../../../../core/components/controls/Button/components/LinkButton";
+import puppy from "../../../../../utils/puppy";
 
-export default () => (
-  <CardIntegratedForColumns>
-    <CardHeader
-      buttons={[0]}
-      stubborn
-      noStar
-      title="Offers, Discounts, Promo Codes"
-    />
-    <CardWithDocketsInfo
-      style={{
-        float: "none",
-        width: "calc(100% - 1em)",
-        height: "7.655em",
-        lineHeight: "1em",
-      }}
-    >
-      <p>
-        <small>
-          <Link
-            style={{ background: c_yellow }}
-            to="https://photoklassik-international.com/shop/ref/29/"
-          >
-            10% Off PhotoKlassik Magazine!
-          </Link>{" "}
-          <em>Use promo code: AnalogCafe at checkout.</em>
-        </small>
-      </p>
-      <p>
-        <small>
-          <Link
-            style={{ background: c_yellow }}
-            to="https://www.etsy.com/ca/shop/FilmBase?coupon=CAFE10"
-          >
-            10% Off Film Cameras
-          </Link>{" "}
-          <em>on Etsy.</em>
-        </small>
-      </p>
-    </CardWithDocketsInfo>
-    <LinkButton href="/offers">Browse All Offers</LinkButton>
-  </CardIntegratedForColumns>
-);
+export default () => {
+  const request = {
+    url: API.ADS,
+    method: "get",
+    params: {
+      location: "account",
+    },
+  };
+  const [adContent, setAdContent] = useState();
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      !adContent &&
+        puppy(request)
+          .then(r => r.json())
+          .then(response => {
+            setAdContent(response);
+          })
+          .catch(() => {});
+    }
+  }, [adContent]);
+
+  return (
+    <CardIntegratedForColumns>
+      <CardHeader
+        buttons={[0]}
+        stubborn
+        noStar
+        title="Offers, Discounts, Promo Codes"
+      />
+      <CardWithDocketsInfo
+        style={{
+          float: "none",
+          width: "calc(100% - 1em)",
+          // height: "7.655em",
+          height: "10.25em",
+          lineHeight: "1em",
+        }}
+      >
+        {adContent &&
+          adContent.items &&
+          adContent.items.map(
+            (item, iterable) =>
+              item.link &&
+              item.title && (
+                <p key={iterable}>
+                  <small>
+                    <Link style={{ background: c_yellow }} to={item.link}>
+                      {item.title}
+                    </Link>
+                    {item.description && (
+                      <>
+                        {" "}
+                        <em>{item.description}</em>
+                      </>
+                    )}
+                  </small>
+                </p>
+              )
+          )}
+      </CardWithDocketsInfo>
+      {/*<LinkButton href="/offers">Browse All Offers</LinkButton>*/}
+    </CardIntegratedForColumns>
+  );
+};
