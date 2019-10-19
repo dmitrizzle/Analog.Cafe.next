00366ea07f4368645abdfb32075123032ac50b52
dmitrizzle <8587882+dmitrizzle@users.noreply.github.com>
2019-10-19 12:09:03 -0700

    Add footer test and lazyload footer

--- core/components/layouts/Main/components/Footer.js
+++ core/components/layouts/Main/components/Footer.js
@@ -2,6 +2,7 @@ import { connect } from "react-redux";
 import { withRouter } from "next/router";
 import React from "react";
 import styled from "styled-components";
+import LazyLoad from "react-lazyload";
 
 import { setModal } from "../../../../store/actions-modal";
 import { title } from "../../../../../constants/styles/typography";
@@ -38,64 +39,66 @@ const Links = styled.div`
 `;
 
 const Footer = props => (
-  <Wrapper>
-    <Link
-      to="#top"
-      onClick={event => {
-        event.preventDefault();
-        window.scroll({
-          top: 0,
-          behavior: "smooth",
-        });
-      }}
-    >
-      <Point style={{ height: "1.25em" }} />
-    </Link>
-    <Links>
-      <em>
-        <Link to="/submit">Submissions</Link>
-      </em>
-      <em>
-        <Link
-          to="/nav/topics"
-          onClick={event => {
-            event.preventDefault();
-            props.setModal(topics(props.router.asPath));
-          }}
-        >
-          Topics
-        </Link>
-      </em>
+  <LazyLoad once offset={300} height={"100%"}>
+    <Wrapper data-cy="Footer">
+      <Link
+        to="#top"
+        onClick={event => {
+          event.preventDefault();
+          window.scroll({
+            top: 0,
+            behavior: "smooth",
+          });
+        }}
+      >
+        <Point style={{ height: "1.25em" }} />
+      </Link>
+      <Links>
+        <em>
+          <Link to="/submit">Submissions</Link>
+        </em>
+        <em>
+          <Link
+            to="/nav/topics"
+            onClick={event => {
+              event.preventDefault();
+              props.setModal(topics(props.router.asPath));
+            }}
+          >
+            Topics
+          </Link>
+        </em>
 
-      <em>
-        <Link to="/">Homepage</Link>
-      </em>
-      <em>
-        <Link to="/about">About</Link>
-      </em>
-      <span>
-        <NavMenu nostyles>Menu</NavMenu>
-      </span>
-    </Links>
-    <Links>
-      <em>
-        <Link to="https://twitter.com/analog_cafe">Twitter</Link>
-      </em>
-      <em>
-        <Link to="http://bit.ly/FeedAnalog">Feedly</Link>
-      </em>
-      <em>
-        <Link to="https://instagram.com/analog_cafe">Instagram</Link>
-      </em>
-    </Links>
-    <Links>
-      <em>
-        <Link to="/account">
-          Your Account <Save style={{ width: "1em" }} />
-        </Link>
-      </em>
-    </Links>
-  </Wrapper>
+        <em>
+          <Link to="/">Homepage</Link>
+        </em>
+        <em>
+          <Link to="/about">About</Link>
+        </em>
+        <span>
+          <NavMenu nostyles>Menu</NavMenu>
+        </span>
+      </Links>
+      <Links>
+        <em>
+          <Link to="https://twitter.com/analog_cafe">Twitter</Link>
+        </em>
+        <em>
+          <Link to="http://bit.ly/FeedAnalog">Feedly</Link>
+        </em>
+        <em>
+          <Link to="https://instagram.com/analog_cafe">Instagram</Link>
+        </em>
+      </Links>
+      <Links>
+        <em>
+          <Link to="/account">
+            Your Account <Save style={{ width: "1em" }} />
+          </Link>
+        </em>
+      </Links>
+    </Wrapper>
+  </LazyLoad>
 );
 
 const mapStateToProps = null;
