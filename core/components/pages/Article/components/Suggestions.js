ddec696ea08401b969b3e2a667a9185d3009d0c3
dmitrizzle <8587882+dmitrizzle@users.noreply.github.com>
2019-10-18 18:52:18 -0700

    Update Suggestions.js

--- core/components/pages/Article/components/Suggestions.js
+++ core/components/pages/Article/components/Suggestions.js
@@ -236,7 +236,7 @@ const Suggestions = props => {
                 });
               }}
             >
-              Buy {props.leadAuthor.title} a Coffee <CoffeeInline />
+              Buy {props.leadAuthor.title} a Coffee<CoffeeInline />
             </LinkButton>
           </CardIntegratedForColumns>
         )}
