2fa9447fbeaa78d71aea83413f3adf9dc63a134c
dmitrizzle <8587882+dmitrizzle@users.noreply.github.com>
2019-10-26 17:51:53 -0700

    Feature/cache (#22)
    * Caches all static content on user device
    
    * Fix dev logic

--- server.js
+++ server.js
@@ -31,6 +31,14 @@ const renderError = (pathExpression, statusCode) => {
 };
 
 app.prepare().then(() => {
+  // cache static assets
+  if (!dev) {
+    server.get(/^\/_next\/static\//, (_, res, nextHandler) => {
+      res.setHeader("Cache-Control", "public, max-age=31536000, immutable");
+      nextHandler();
+    });
+  }
+
   // handle GET request to /service-worker.js
   const sw = "/service-worker.js";
   server.get(sw, (req, res) => {
