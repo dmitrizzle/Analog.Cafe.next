8ec502abf0aa2f2c3a3cab6396e8ba4d34666f9a
dmitrizzle <8587882+dmitrizzle@users.noreply.github.com>
2019-10-27 10:47:09 -0700

    Fix/animation (#28)
    * Reduce animation
    
    * Clean up
    
    * Fix download cta

--- core/components/vignettes/HeaderLarge/components/HeaderSubtitle.js
+++ core/components/vignettes/HeaderLarge/components/HeaderSubtitle.js
@@ -1,11 +1,6 @@
-import styled, { css, keyframes } from "styled-components";
+import styled, { css } from "styled-components";
 
 import { title } from "../../../../../constants/styles/typography";
-
-const animationSubtitleFlip = keyframes`
-  from { transform: perspective(10em) rotateX(-90deg)}
-  to { transform:perspective(400px) rotateX(0deg) }
-`;
 
 export const subtitleStyles = css`
   ${title}
@@ -14,16 +9,7 @@ export const subtitleStyles = css`
 
 export const headerSubtitleStyles = css`
   ${subtitleStyles};
-  animation: ${animationSubtitleFlip} 550ms 100ms
-    cubic-bezier(0.46, 0.88, 0.37, 1.43);
-  ${props =>
-    props.isLoading &&
-    css`
-      animation: none;
-    `}
 `;
 export default styled.h2`
   ${headerSubtitleStyles};
 `;
-
-// const [count, setCount] = useState(0);
