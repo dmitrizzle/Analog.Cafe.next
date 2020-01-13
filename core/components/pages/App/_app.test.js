732c649f055555aa20faffcbb79588f1a938db31
dmitrizzle <8587882+dmitrizzle@users.noreply.github.com>
2020-01-12 17:01:32 -0800

    Jest works with typefaces

---
+++ core/components/pages/App/_app.test.js
@@ -0,0 +1,13 @@
+import "jest-styled-components";
+
+import React from "react";
+
+import { shallow } from "enzyme";
+
+import App from "../../../../pages/_app.js";
+
+describe("`_app.js` implementation tests", () => {
+  it("", () => {
+    const app = shallow(<App />);
+  });
+});
