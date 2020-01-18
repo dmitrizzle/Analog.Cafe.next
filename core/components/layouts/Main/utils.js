7747b3d0f32ec53e60d40c831f078f88b9b5a50b
dmitrizzle <8587882+dmitrizzle@users.noreply.github.com>
2020-01-18 12:52:20 -0800

    Add files

---
+++ core/components/layouts/Main/utils.js
@@ -0,0 +1,21 @@
+import { NAV_CONFIG } from "./constants";
+import { NAV_MIN_MAP } from "../../../../constants/router/breadcrumbs";
+
+export const mapPathnameToNavConfig = pathname => {
+  const isMinimalNavigation =
+    NAV_MIN_MAP[
+      Object.keys(NAV_MIN_MAP).filter(key => pathname.indexOf(key) !== -1)[0]
+    ];
+
+  const has = string => pathname.indexOf(string) !== -1;
+
+  if (pathname === "/") return NAV_CONFIG.LIST;
+
+  if (has("/write/upload")) return NAV_CONFIG.HIDDEN;
+
+  if (has("/account/all-submissions")) return NAV_CONFIG.LIST;
+  if (has("/account/bookmarks")) return NAV_CONFIG.LIST;
+  if (has("/account/submission")) return NAV_CONFIG.DEFAULT;
+
+  return isMinimalNavigation ? NAV_CONFIG.MINIMAL : NAV_CONFIG.DEFAULT;
+};
