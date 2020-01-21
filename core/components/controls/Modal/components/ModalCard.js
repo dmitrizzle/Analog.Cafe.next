5b505e1b32e97cdf3f74c5d7df2a60abaecd9d9a
dmitrizzle <8587882+dmitrizzle@users.noreply.github.com>
2020-01-20 22:39:07 -0800

    Start building ad section for modal

--- core/components/controls/Modal/components/ModalCard.js
+++ core/components/controls/Modal/components/ModalCard.js
@@ -1,7 +1,44 @@
 import React from "react";
 
 import Card from "../../Card";
+import Label from "../../../vignettes/Label";
 
-export default props => (
-  <Card {...props} style={{ margin: `3em auto 90.1vh` }} id="modal-card" />
-);
+//90.1vh
+export default props => {
+  console.log(props);
+  return (
+    <>
+      <Card {...props} style={{ margin: `3em auto 0` }} id="modal-card" />
+      <Card
+        noStar
+        buttons={[
+          <Label
+            style={{
+              display: "block",
+              margin: ".5em 0",
+              height: "1em",
+            }}
+          >
+            Partner
+          </Label>,
+          {
+            text: "Shop Now",
+            branded: true,
+            to: "https://analoguewonderland.co.uk/?p=rJutywT1L",
+          },
+        ]}
+        text={
+          <>
+            Analogue Wonderland is our{" "}
+            <strong>trusted community partner.</strong> They sell a wide
+            selection film at fair prices with excellent service.
+          </>
+        }
+        stubborn
+        title="Analogue Wonderland"
+        style={{ margin: `3em auto 90.1vh` }}
+        image="image-froth_1610000_r1WbC_bgQ"
+      />
+    </>
+  );
+};
