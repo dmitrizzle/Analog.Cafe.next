111b33e5a56e0c4ac1c40bb59a3c7bcc445aaaa7
dmitrizzle <8587882+dmitrizzle@users.noreply.github.com>
2020-01-31 22:00:37 +0300

    Add clear cache command

--- utils/server-cache.js
+++ utils/server-cache.js
@@ -1,11 +1,28 @@
 import { DOMAIN } from "../constants/router/defaults";
 import { ROUTE_TAGS } from "../core/components/pages/List/constants";
 
-export const invalidate = url => fetch(url + "?force=true", { method: "get" });
+const p = process.env.NODE_ENV === "production" ? "PRODUCTION" : "DEVELOPMENT";
+
+export const invalidate = url => {
+  if (!url) return;
+  // force cache clear on Next.js server
+  fetch(url + "?force=true", { method: "get" }).then(() => {
+    // clear CloudFlare caches
+    // process.env.NODE_ENV === "production"
+    puppy({
+      url: `${DOMAIN.PROTOCOL[p] + DOMAIN.API[p]}/admin/cache`,
+      method: "DELETE",
+      headers: {
+        Authorization: "JWT " + ls.getItem("token"),
+      },
+      data: {
+        files: [url],
+      },
+    });
+  });
+};
 
 export const invalidateArticlePages = article => {
-  const p =
-    process.env.NODE_ENV === "production" ? "PRODUCTION" : "DEVELOPMENT";
   const base = DOMAIN.PROTOCOL[p] + DOMAIN.APP[p];
 
   // clear all lists
