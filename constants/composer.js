916856390f4850e6773fd21ed8f3e5d0c3420e51
dmitrizzle <8587882+dmitrizzle@users.noreply.github.com>
2020-02-01 10:04:16 +0300

    Copy fix

--- constants/composer.js
+++ constants/composer.js
@@ -64,20 +64,23 @@ export const HINTS = {
       image:
         "https://res.cloudinary.com/analog-cafe/image/upload/v1528904759/image-froth_1010453_425a5704760c4879b31e008315c3047c",
       text: () => (
-        <span>
+        <>
           <strong>Analog.Cafe Composer</strong> makes submissions easy. No more
           endless email and document uploads!
           <br />
           <br />
-          Easily add <u>links</u> and <strong>font</strong>{" "}
+          What you see in the editor is exactly what your article will look like
+          when published.
+          <br />
+          <br />
+          Add <u>links</u> and <strong>font</strong>{" "}
           <span style={{ fontStyle: "normal" }}>styles</span>. Format your
           titles, quotes, and images.
           <br />
           <br />
-          You will know exactly what your article will look like published. See
-          who (from Analog.Cafe editorial team) and when edited your article.
-          Get notified once the article is published or rejected.
-        </span>
+          See who and when edited your article. Get notified once your work is
+          published or rejected.
+        </>
       ),
       buttons: [
         {
