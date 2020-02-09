ff6a5142f369de1408e015ef1f03bc65e1b6238f
dmitrizzle <8587882+dmitrizzle@users.noreply.github.com>
2020-02-08 22:33:13 -0800

    Update date modified

--- apps/35mm-film-price-guide/constants.js
+++ apps/35mm-film-price-guide/constants.js
@@ -3,7 +3,7 @@ import { makeFroth } from "../../utils/froth";
 
 export const DATE = {
   published: 1541376000,
-  modified: 1578636325,
+  modified: 1581229977,
 };
 export const FILM_PRICE_DATA = [
   {
