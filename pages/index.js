f5c6d6aacc1412390cf73529019129e238bbb4cc
dmitrizzle <8587882+dmitrizzle@users.noreply.github.com>
2020-02-18 13:18:03 -0800

    Remove features

--- pages/index.js
+++ pages/index.js
@@ -9,32 +9,24 @@ import { fetchListPage } from "../core/store/actions-list";
 import { getListMeta } from "../core/components/pages/List/utils";
 import { withRedux } from "../utils/with-redux";
 import Error from "./_error";
-import Features from "../core/components/controls/Features";
 import List from "../core/components/pages/List";
 import Main from "../core/components/layouts/Main";
 
 const Index = props => {
-  const { list, listFeatures, query, isSsr } = props;
+  const { list, query, isSsr } = props;
   if (isSsr) {
     // clear old cache for seen pages beyond 1
     if (props.requests) cleanListPageCaches(props.requests.list);
 
     // refresh cache for list data
     responseCache.set(props.requests.list, list);
-    responseCache.set(props.requests.features, listFeatures);
   }
 
   return props.error ? (
     <Error statusCode={500} />
   ) : (
     <Main>
-      <Features
-        listFeatures={listFeatures}
-        listTag={list.filter.tags[0]}
-        activeCollection={query?.collection}
-        isActiveTag={list?.filter.tags.length > 0}
-      />
-      <List list={list} listFeatures={listFeatures} />
+      <List list={list} />
     </Main>
   );
 };
@@ -53,10 +45,7 @@ Index.getInitialProps = async ({ reduxStore, pathname, res, query, req }) => {
   // list items
   await reduxStore.dispatch(fetchListPage(listRequest));
 
-  // featured items
-  await reduxStore.dispatch(fetchListFeatures());
-
-  const { list, listFeatures } = reduxStore.getState();
+  const { list } = reduxStore.getState();
 
   // 500
   if (list.status === "error" || list.error) {
@@ -66,10 +55,9 @@ Index.getInitialProps = async ({ reduxStore, pathname, res, query, req }) => {
 
   return {
     list,
-    listFeatures,
     query,
     isSsr: !!req,
-    requests: { list: listRequest, features: requestFeatured },
+    requests: { list: listRequest },
   };
 };
 
