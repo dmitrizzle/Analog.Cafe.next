5b309ce45d0f93895dafc6f70baceb2326edb520
dmitrizzle <8587882+dmitrizzle@users.noreply.github.com>
2020-03-14 16:51:05 -0700

    Update shop description for filmbase shop

--- pages/app/35mm-film-price-guide.js
+++ pages/app/35mm-film-price-guide.js
@@ -447,21 +447,27 @@ const AppPriceGuide = props => {
                             with={{
                               info: {
                                 title: "Community Referral",
-                                text: (
-                                  <>
-                                    <strong>
-                                      Analog.Cafe chose to endorse{" "}
-                                      {item.referralShopName} products because
-                                      we are their customer.
-                                    </strong>{" "}
-                                    We’ve tried and trust their service. If you
-                                    choose to purchase from{" "}
-                                    {item.referralShopName}, a small percentage
-                                    of a sale will come back to Analog.Cafe — at
-                                    no extra cost to you. Your support is
-                                    appreciated!
-                                  </>
-                                ),
+                                text:
+                                  item.referralShopName === "FilmBase" ? (
+                                    <>
+                                      FilmBase is Analog.Cafe’s Etsy shop where
+                                      we sell premium, tested film cameras.
+                                    </>
+                                  ) : (
+                                    <>
+                                      <strong>
+                                        Analog.Cafe chose to endorse{" "}
+                                        {item.referralShopName} products because
+                                        we are their customer.
+                                      </strong>{" "}
+                                      We’ve tried and trust their service. If
+                                      you choose to purchase from{" "}
+                                      {item.referralShopName}, a small
+                                      percentage of a sale will come back to
+                                      Analog.Cafe — at no extra cost to you.
+                                      Your support is appreciated!
+                                    </>
+                                  ),
                               },
                               id: "help/affiliate",
                             }}
