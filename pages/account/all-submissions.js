2583ffc47502deb596d908818556222d40d1db7d
dmitrizzle <8587882+dmitrizzle@users.noreply.github.com>
2020-06-19 18:23:41 -0700

    Submissions button

--- pages/account/all-submissions.js
+++ pages/account/all-submissions.js
@@ -1,12 +1,24 @@
 import { useDispatch, useSelector } from "react-redux";
 import React from "react";
+import styled from "styled-components";
 
+import { b_phablet } from "../../constants/styles/measurements";
 import { initListPage } from "../../core/store/actions-list";
 import { withRedux } from "../../utils/with-redux";
+import ArticleWrapper from "../../core/components/pages/Article/components/ArticleWrapper";
 import ClientLoader from "../../core/components/layouts/Main/components/ClientLoader";
 import Error from "../_error";
+import HeaderLarge from "../../core/components/vignettes/HeaderLarge";
+import LinkButton from "../../core/components/controls/Button/components/LinkButton";
 import List from "../../core/components/pages/List";
 import Main from "../../core/components/layouts/Main";
+import ls from "../../utils/storage/ls";
+
+const ComposerButton = styled(LinkButton)`
+  @media (max-width: 488px) {
+    display: none;
+  }
+`;
 
 const Submissions = () => {
   const { status } = useSelector(state => state.user);
@@ -26,6 +38,14 @@ const Submissions = () => {
     <Error statusCode={403} />
   ) : (
     <Main title="Submissions">
+      <ArticleWrapper style={{ height: "9.75em" }}>
+        <HeaderLarge pageTitle="Your Submissions" />
+        <ComposerButton to="/write/draft">
+          {ls.getItem("composer-content-text")
+            ? "Edit Article Draft"
+            : "Write/Submit Your Article"}
+        </ComposerButton>
+      </ArticleWrapper>
       <List private={true} />
     </Main>
   );
