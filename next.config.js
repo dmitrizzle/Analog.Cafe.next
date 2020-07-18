2134f7cf31732cd2d12ac4bc5be83055dd33bca3
dmitrizzle <8587882+dmitrizzle@users.noreply.github.com>
2020-07-18 11:09:12 -0700

    Fix bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-const offline = require("next-pwa");
 const withPlugins = require("next-compose-plugins");
+const offline = require("next-pwa");
 const css = require("@zeit/next-css"); // required, otherwise fonts won't work
+const bundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
 
 // next config for general options
 const nextConfig = {
@@ -64,4 +67,9 @@ const offlineConfig = {
   },
 };
 
-module.exports = withPlugins([offline, offlineConfig], [css, {}], nextConfig);
+module.exports = withPlugins(
+  [bundleAnalyzer, {}],
+  [offline, offlineConfig],
+  [css, {}],
+  nextConfig
+);
