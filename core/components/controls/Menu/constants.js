ba33b7a6b82aa2de13c87392b95d89cf4f7d4363
dmitrizzle <8587882+dmitrizzle@users.noreply.github.com>
2020-07-19 10:14:48 -0700

    Remove unused

--- core/components/controls/Menu/constants.js
+++ core/components/controls/Menu/constants.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
 import { ROUTE_LABELS } from "../../pages/List/constants";
 import { bookmarksModal } from "../Features/components/PosterBookmarks";
