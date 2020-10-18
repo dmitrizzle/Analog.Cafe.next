a5f8206c4232e7f294ed9003a3a9886be29e9d8b
dmitrizzle <8587882+dmitrizzle@users.noreply.github.com>
2020-10-18 02:03:29 -0700

    add todo

--- core/components/controls/Theme/utils.js
+++ core/components/controls/Theme/utils.js
@@ -3,6 +3,8 @@ import { themeInitialState } from "../../../store/reducers-theme";
 export const autoTheme = () => {
   if (!process.browser) return themeInitialState;
 
+  // TODO: switch the theme in real-time as system prefs change
+
   const browserTheme =
     window.matchMedia &&
     window.matchMedia("(prefers-color-scheme: dark)").matches
