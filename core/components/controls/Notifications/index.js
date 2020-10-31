2b3687553ebf0df70193673ff424aa89bb12aca2
dmitrizzle <8587882+dmitrizzle@users.noreply.github.com>
2020-10-31 14:55:15 -0700

    Refactor and target messages based on urls

--- core/components/controls/Notifications/index.js
+++ core/components/controls/Notifications/index.js
@@ -4,6 +4,7 @@ import styled, { css } from "styled-components";
 import throttle from "lodash.throttle";
 
 import { API } from "../../../../constants/router/defaults";
+import { NotificationsWrapper } from "./components/NotificationsWrapper";
 import {
   b_mobile,
   m_radius_sm,
@@ -14,113 +15,18 @@ import {
   notificationDismiss,
   notificationShow,
 } from "../../../../constants/styles/animation";
+import { getContentGroupName } from "./utils";
 import { makeFroth } from "../../../../utils/froth";
 import { title } from "../../../../constants/styles/typography";
 import ga from "../../../../utils/data/ga";
 import puppy from "../../../../utils/puppy";
 
-const NotificationsWrapper = styled.aside`
-  display: block;
-  position: fixed;
-  z-index: 31;
-  width: 100%;
-  top: 0;
-  left: 0;
-  padding: ${({ isMini }) => (isMini ? 0 : 0.25)}em 0 0;
-  cursor: pointer;
-  transform: scale(0, 0) translateZ(0);
-  animation: ${({ hasMessage, messageDismissed }) => {
-      if (hasMessage && !messageDismissed) return notificationShow;
-      if (hasMessage && messageDismissed) return notificationDismiss;
-      return "none";
-    }}
-    500ms ease forwards;
-
-  > div {
-    display: flex;
-    align-items: center;
-
-    margin: 0 auto;
-
-    background: ${({ theme }) => theme.brand};
-    border-radius: ${m_radius_sm};
-
-    ${({ isMini }) => {
-      if (isMini)
-        return css`
-          height: 1.33em;
-          border-radius: 0;
-          max-width: 100%;
-          box-shadow: 0 0 0 0 ${c_charcoal};
-          justify-content: center;
-        `;
-      return css`
-        height: 2.775em;
-        border-radius: ${m_radius_sm};
-        max-width: ${b_mobile};
-        box-shadow: 0 0 0 1px ${c_charcoal};
-        justify-content: start;
-      `;
-    }}
-    transition: all 250ms;
-
-    > div {
-      opacity: 0;
-      animation: ${fadeIn} 500ms 500ms ease forwards;
-      transition: all 250ms;
-
-      color: ${({ theme }) => theme.bg};
-
-      font-size: 0.8em;
-      text-align: left;
-      white-space: nowrap;
-      overflow: hidden;
-      text-overflow: ellipsis;
-
-      margin: 0 0.5em;
-      ${({ isMini }) => {
-        if (isMini)
-          return css`
-            margin: 0 0;
-            padding: 0 0.5em;
-          `;
-      }}
-
-      line-height: 1.25em;
-      > span {
-        ${title};
-      }
-    }
-    > figure {
-      ${({ isMini }) => {
-        if (isMini)
-          return css`
-            width: 0;
-            height: 0;
-            margin: 0;
-          `;
-        return css`
-          width: 2em;
-          height: 2em;
-          margin: 0em 0 0 0.35em;
-        `;
-      }}
-      transition: all 150ms;
-
-      overflow: hidden;
-      border-radius: ${m_radius_sm};
-
-      img {
-        width: 100%;
-      }
-    }
-  }
-`;
-
 const Notifications = ({ router }) => {
   const [messages, setMessages] = useState([]);
   const [status, setStatus] = useState("pending");
   const [messageDismissed, setMessageDismissed] = useState(false);
+
+  // fetch data
   useEffect(() => {
     if (status !== "pending") return;
     puppy({
@@ -140,12 +46,36 @@ const Notifications = ({ router }) => {
       });
   }, [messages]);
 
-  let title, description, link, poster;
-  if (messages[0]) {
-    title = messages[0].title;
-    description = messages[0].description;
-    link = messages[0].link;
-    poster = messages[0].poster;
+  // apply targeting
+  const [targetedMessages, setTargetedMessages] = useState([]);
+  useEffect(() => {
+    setTargetedMessages(
+      messages.map(message => {
+        // not specifying target applies message to everything
+        if (!message.target) return { ...message, target: { match: true } };
+
+        // test targeting and mark messages with results
+        return {
+          ...message,
+          target: {
+            ...message.target,
+            match:
+              message.target.contentGroups?.indexOf(
+                getContentGroupName(router.asPath)
+              ) > -1,
+          },
+        };
+      })
+    );
+  }, [router.asPath]);
+
+  let title, description, link, poster, targetMatch;
+  if (targetedMessages.length) {
+    title = targetedMessages[0].title;
+    description = targetedMessages[0].description;
+    link = targetedMessages[0].link;
+    poster = targetedMessages[0].poster;
+    targetMatch = targetedMessages[0].target?.match;
   }
 
   // change notification size based on scroll position
@@ -155,6 +85,7 @@ const Notifications = ({ router }) => {
     else return setNotificationSizeMini(true);
   }, 100);
 
+  // scroll design transition
   useEffect(() => {
     window.addEventListener("scroll", windowScrollHandlerNotifications, true);
     return () => {
@@ -167,42 +98,39 @@ const Notifications = ({ router }) => {
   }, []);
 
   return (
-    <>
-      <NotificationsWrapper
-        isMini={isMini}
-        hasMessage={title}
-        sticky={false}
-        messageDismissed={messageDismissed}
-        onClick={() => {
-          ga("event", {
-            category: link.indexOf("http") === 0 ? "out" : "nav",
-            action: "message.click",
-            label: link,
-          });
-          setTimeout(() => {
-            if (link.indexOf("http") === 0) {
-              const newTab = window.open(link, "_blank");
-              newTab.focus();
-              return;
-            }
-            window.scrollTo && window.scrollTo({ top: 0, behavior: "smooth" });
-            router.push(link);
-          }, 750);
-          setMessageDismissed(true);
-        }}
-      >
+    <NotificationsWrapper
+      isMini={isMini}
+      hasMessage={title}
+      messageDismissed={messageDismissed || !targetMatch}
+      onClick={() => {
+        ga("event", {
+          category: link.indexOf("http") === 0 ? "out" : "nav",
+          action: "message.click",
+          label: link,
+        });
+        setTimeout(() => {
+          if (link.indexOf("http") === 0) {
+            const newTab = window.open(link, "_blank");
+            newTab.focus();
+            return;
+          }
+          window.scrollTo && window.scrollTo({ top: 0, behavior: "smooth" });
+          router.push(link);
+        }, 750);
+        setMessageDismissed(true);
+      }}
+    >
+      <div>
+        <figure>
+          <img src={makeFroth({ src: poster, size: "i", type: "jpg" }).src} />
+        </figure>
         <div>
-          <figure>
-            <img src={makeFroth({ src: poster, size: "i", type: "jpg" }).src} />
-          </figure>
-          <div>
-            <em>{title}</em>
-            {isMini ? " " : <br />}
-            <span>{description}</span>
-          </div>
+          <em>{title}</em>
+          {isMini ? " " : <br />}
+          <span>{description}</span>
         </div>
-      </NotificationsWrapper>
-    </>
+      </div>
+    </NotificationsWrapper>
   );
 };
 
