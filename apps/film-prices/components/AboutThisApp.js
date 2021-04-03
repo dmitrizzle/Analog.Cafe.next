8584b03424de7aff824b57b583fabb8e19a03885
dmitrizzle <8587882+dmitrizzle@users.noreply.github.com>
2021-04-03 15:04:11 -0700

    Desc

--- apps/film-prices/components/AboutThisApp.js
+++ apps/film-prices/components/AboutThisApp.js
@@ -5,16 +5,15 @@ const AboutThisApp = () => (
     <h3>What is this?</h3>
     <p>
       <strong>Film Prices</strong> is a searchable database of popular 35mm film
-      stocks with samples, prices ($, €, £, ¥, ฿), and mini-reviews. Use this
-      web app to save money and get better results from your 35mm film
-      photography.
+      stocks with <strong>industry average price history</strong> (since 2018)
+      in $, €, £, ¥, and ฿. Use this web app to save money and get better
+      results from your 35mm film photography.
     </p>
     <h3>How does it work?</h3>
     <p>
-      I’ve been maintaining this database of popular 35mm film prices, samples,
-      and mini-reviews since 2018. This app has helped many people, including
-      myself, understand what a “fair”/market/average price of a film is and
-      what kind of result to expect from it.
+      This app has helped many people, including myself, understand what a
+      “fair”/market/average price of a film is and what kind of result to expect
+      from it.
     </p>
     <p>
       To get started, search for your film stock and click on it for more
