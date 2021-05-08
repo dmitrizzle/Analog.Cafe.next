945c49b9f47606a92aaa9944d98578907d330eac
dmitrizzle <8587882+dmitrizzle@users.noreply.github.com>
2021-05-08 14:44:07 -0700

    lint

--- core/components/controls/Link/components/LinkDecorators.js
+++ core/components/controls/Link/components/LinkDecorators.js
@@ -1,11 +1,10 @@
 import Link from "next/link";
+import React from "react";
 import styled, { css } from "styled-components";
 
 import { c_red } from "../../../../../constants/styles/themes";
 
-export const ExternalLink = styled(({ height, width, ...props }) => (
-  <Link {...props} />
-))`
+export const ExternalLink = styled(({ ...props }) => <Link {...props} />)`
   ${({ router }) =>
     (router.asPath.includes("/r/") ||
       router.asPath.includes("/account/submission")) &&
