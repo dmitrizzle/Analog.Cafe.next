e984312977bb70365aa9ef5cff484a120b35b33b
dmitrizzle <8587882+dmitrizzle@users.noreply.github.com>
2021-06-11 22:56:57 -0700

    Bump style on login buttons

--- user/components/pages/Account/components/FormElements.js
+++ user/components/pages/Account/components/FormElements.js
@@ -12,11 +12,12 @@ export const TwitterButton = styled(LinkButton)`
   background: #1da1f2;
   box-shadow: 0 0 0 1px #1da1f2;
   color: ${c_white} !important;
+  text-align: left;
   svg {
     height: 2em;
-    margin: -1em 0 -1em -1.75em;
+    margin: -1em 0.25em -1em 0.75em;
     @media (min-width: ${b_movie}) {
-      margin: -1em 0em -1em -0.5em;
+      margin: -1em 0.25em -1em 0.5em;
     }
   }
 `;
@@ -24,11 +25,12 @@ export const FacebookButton = styled(LinkButton)`
   background: #4267b2;
   box-shadow: 0 0 0 1px #4267b2;
   color: ${c_white} !important;
+  text-align: left;
   svg {
     height: 2em;
-    margin: -1em 0 -1em -1.75em;
+    margin: -1em 0.25em -1em 0.75em;
     @media (min-width: ${b_movie}) {
-      margin: -1em -0.5em;
+      margin: -1em 0.25em -1em 0.5em;
     }
   }
 `;
