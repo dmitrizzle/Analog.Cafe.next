ed9dc993eaa2ba11fcc17bb09444cfb50ffea45d
dmitrizzle <8587882+dmitrizzle@users.noreply.github.com>
2021-07-08 17:33:57 -0700

    Currency update

--- constants/currency.js
+++ constants/currency.js
@@ -1,5 +1,5 @@
 export const CURRENCY = {
-  EXCHANGE: { cad: 1, usd: 0.81, eur: 0.67, gbp: 0.58, jpy: 87, thb: 25.33 },
+  EXCHANGE: { cad: 1, usd: 0.81, eur: 0.68, gbp: 0.58, jpy: 90, thb: 25.84 },
   SYMBOL: {
     cad: "$",
     usd: "$",
